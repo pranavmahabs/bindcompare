efb477922eaad0b32440fb97d72882ceac698b69
pranavmahabs <90987389+pranavmahabs@users.noreply.github.com>
2023-12-31 17:15:14 +0530

    SHIFT: BedTools to SeqIO for Arm

--- bindapp/downstream.py
+++ bindapp/downstream.py
@@ -10,66 +10,88 @@
 
 import os
 import pandas as pd
-import pybedtools as bd
+from Bio import SeqIO
 import sys
 import subprocess
 
-if sys.argv[2] == "None":
-    os.write(2, b"Skipping Sequence Extraction and Motif Analysis... Beginning GO!\n")
-else:
-    os.write(2, b"Completed BED Merge... starting downstream analysis!\n")
-    input_genes = sys.argv[1]
-    df = pd.read_csv(input_genes)
-    bed = df[["Chrom", "Begin Ref Site", "End Ref Site"]]
 
-    fasta = bd.example_filename(sys.argv[2])
-    a = bd.BedTool.from_dataframe(bed)
-    a = a.sequence(fi=fasta)
-
-    data = open(a.seqfn).read().splitlines()
-    sequences = list(filter(lambda seq: seq[0] != ">", data))
-    df["Sequences"] = sequences
-    # df = df.drop(columns=["Unnamed: 0"])
-    df.to_csv(input_genes)
-
-    outdir = sys.argv[3]
-    fastapath = outdir + "/sequences.fasta"
-    # Create the FASTA file of extracted sequences
-    fp = open(fastapath, "w")
-    for _, row in df.iterrows():
-        chrom_pos = (
-            row["Chrom"] + str(row["Begin Ref Site"]) + ":" + str(row["End Ref Site"])
+def get_chrom2seq(FASTA_FILE, capitalize=True):
+    """
+    Load in the genome fasta file to extract sequences from the BED Files.
+    """
+    chrom2seq = {}
+    for seq in SeqIO.parse(FASTA_FILE, "fasta"):
+        chrom2seq[seq.description.split()[0]] = (
+            seq.seq.upper() if capitalize else seq.seq
         )
-        name = (
-            ">"
-            + row["GeneIDs"].replace('"', "").replace(";", "")
-            + ":"
-            + chrom_pos
-            + "\n"
-        )
-        fp.write(name)
-        sequence = row["Sequences"] + "\n"
-        fp.write(sequence)
-    fp.close()
+    return chrom2seq
 
-    # Need to use STREME if num_sequences > 50:
-    if len(df) > 50:
-        cmd_str = (
-            "streme -p "
-            + fastapath
-            + " --dna --nmotifs 10 --oc "
-            + outdir
-            + "/motifanalysis 2> /dev/null"
-        )
+
+def downstream():
+    if sys.argv[2] == "None":
+        os.write(2, b"Skipping Sequence Extraction and Motif Analysis...\n")
     else:
-        cmd_str = (
-            "meme -p"
-            + fastapath
-            + " --dna --nmotifs 5 --maxw 50 --oc "
-            + outdir
-            + "/motifanalysis 2> /tmp/out.txt"
-        )
-    # Run STREME or MEME and produce the motif analysis
-    subprocess.run(cmd_str, shell=True, stdout=subprocess.DEVNULL)
+        os.write(2, b"Completed BED Merge... starting downstream analysis!\n")
+        input_genes = sys.argv[1]
+        df = pd.read_csv(input_genes)
+        bed = df[["Chrom", "Begin Ref Site", "End Ref Site"]]
 
-    os.write(2, b"Finished Sequence Extraction and MEME/STREME... Beginning GO!\n")
+        fasta = sys.argv[2]
+        chrom2seq = get_chrom2seq(fasta)
+
+        df["Sequences"] = df.apply(
+            lambda row: str(
+                chrom2seq[row.Chrom][row["Begin Ref Site"] : row["End Ref Site"]]
+            ),
+            axis=1,
+        )
+        df.to_csv(input_genes)
+
+        outdir = sys.argv[3]
+        fastapath = outdir + "/sequences.fasta"
+        # Create the FASTA file of extracted sequences
+        fp = open(fastapath, "w")
+        for _, row in df.iterrows():
+            chrom_pos = (
+                row["Chrom"]
+                + str(row["Begin Ref Site"])
+                + ":"
+                + str(row["End Ref Site"])
+            )
+            name = (
+                ">"
+                + row["GeneIDs"].replace('"', "").replace(";", "")
+                + ":"
+                + chrom_pos
+                + "\n"
+            )
+            fp.write(name)
+            sequence = row["Sequences"] + "\n"
+            fp.write(sequence)
+        fp.close()
+
+        # Need to use STREME if num_sequences > 50:
+        if len(df) > 50:
+            cmd_str = (
+                "streme -p "
+                + fastapath
+                + " --dna --nmotifs 10 --oc "
+                + outdir
+                + "/motifanalysis 2> /dev/null"
+            )
+        else:
+            cmd_str = (
+                "meme -p"
+                + fastapath
+                + " --dna --nmotifs 5 --maxw 50 --oc "
+                + outdir
+                + "/motifanalysis 2> /tmp/out.txt"
+            )
+        # Run STREME or MEME and produce the motif analysis
+        # subprocess.run(cmd_str, shell=True, stdout=subprocess.DEVNULL)
+
+        # os.write(2, b"Finished Sequence Extraction and MEME/STREME... Beginning GO!\n")
+
+
+if __name__ == "__main__":
+    downstream()
