9222737f9c2250a35c98e0c3e3fd1d0f0cadac5d
pranavmahabs <90987389+pranavmahabs@users.noreply.github.com>
2024-01-22 17:56:23 -0500

    VERSION 1.1

--- bindcompare/__init__.py
+++ bindcompare/__init__.py
@@ -4,6 +4,6 @@ bindcompare
 A Novel DNA/RNA Binding Integration and Analysis Platform.
 """
 
-__version__ = "1.0.1"
+__version__ = "1.1.0"
 __author__ = "Pranav Mahableshwarkar"
 __credits__ = "Larschan Lab at Brown University"
